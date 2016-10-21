5d3954547be8bc6a16deaa42b12400bdd66cb824
student <student@521-15>
2016-10-21 13:19:43 +0200

    I like ham.

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author student
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int broj = 1;
+        broj = broj + 5;
+        System.out.println(broj);
+    }
+}
